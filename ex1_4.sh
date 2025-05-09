60c6bbf5fe3dfbf9ab86c0fd8efb4e4b35b4dcd3
GIT-SH-J <ishj250123@gmail.com>
2025-05-09 19:45:36 +0900

    log 파일 개수 추가

--- ex1_4.sh
+++ ex1_4.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
+
 cd ~
-mkdir -p logs/app.log 
-mkdir -p logs/erro.log
+
 mkdir -p logs/old_logs
+
+touch logs/app1.log
+touch logs/error.log
 touch logs/old_logs/app_old.log
 
 find ~/logs -type f -name "*.log"
+
+find ~/logs -type f -name "*.log" | wc -l
